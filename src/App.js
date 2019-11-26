bc26b57171cf16a2e0320aee2e4bb37167bdfd77
Jeremy Bernhardt <bernhardtjeremy@gmail.com>
2019-11-26 10:56:35 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import tachibana from './bg.jpg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        [ <a href="http://lain.gboards.ca/pub/music">Files</a> |
+        <a href="mailto:bernhardtjeremy@gmail.com">Become a DJ!</a> |
+        <a href="http://gboards.ca/">Feed Me.</a> ]
+      </div>
+      <img alt="tachibani Inc logo" src={tachibana}/>
+      <div className='jj'>
+      <audio xmlns="http://www.w3.org/1999/xhtml" controls="controls" preload="none">
+        <source src="http://jj.gboards.ca/cyberia.ogg" type="application/ogg" /></audio>
+      </div>
     </div>
   );
 }
