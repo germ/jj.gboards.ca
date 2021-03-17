f592debb26739147d13b87588095423004222db6
Jane Bernhardt <jane@gboards.ca>
2021-03-16 23:55:52 -0600

    yee

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './App.css';
-import tachibana from './bg.jpg'
+import tachibana from './bio.png'
 
 function App() {
   return (
     <div className="App">
+
+      <img alt="bio" src={tachibana}/>
+
       <div className="header">
-        [ <a href="http://lain.gboards.ca/pub/music">Files</a> |
-        <a href="mailto:bernhardtjeremy@gmail.com">Become a DJ!</a> |
-        <a href="http://gboards.ca/">Feed Me.</a> ]
+        Mask Up, Pair Up, Press Play.
       </div>
-      <img alt="tachibani Inc logo" src={tachibana}/>
+
       <div className='jj'>
-      <audio xmlns="http://www.w3.org/1999/xhtml" controls="controls" preload="none">
-        <source src="http://jj.gboards.ca/cyberia.ogg" type="application/ogg" /></audio>
+      <audio id="play" xmlns="http://www.w3.org/1999/xhtml" controls="controls" preload="none">
+        <source src="http://jj.gboards.ca/canyounot.ogg" type="application/ogg" /></audio>
       </div>
     </div>
   );
